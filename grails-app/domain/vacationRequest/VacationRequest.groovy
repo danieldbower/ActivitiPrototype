16b57f7cdea92dcac362e5e243fa405df3acf34d
Daniel Bower <daniel.bower@infinum.com>
2013-11-13 15:28:21 -0500

    minor cosmetics

--- grails-app/domain/vacationRequest/VacationRequest.groovy
+++ grails-app/domain/vacationRequest/VacationRequest.groovy
@@ -14,10 +14,9 @@
  */
 package vacationRequest
 
-import org.grails.activiti.ApprovalStatus  
-  
-  /**
- *
+import org.grails.activiti.ApprovalStatus
+
+/**
  * @author <a href='mailto:limcheekin@vobject.com'>Lim Chee Kin</a>
  *
  * @since 5.0.beta2
@@ -27,12 +26,12 @@ class VacationRequest {
 	Integer numberOfDays
 	String vacationDescription
 	ApprovalStatus approvalStatus = ApprovalStatus.PENDING
-	String approvalRemark 
+	String approvalRemark
 	Boolean resendRequest
-  Date dateCreated
-  Date lastUpdated
-  	
-  static constraints = {
+	Date dateCreated
+	Date lastUpdated
+
+	static constraints = {
 		employeeName blank:false, size:5..50
 		numberOfDays range:1..14
 		vacationDescription blank:false, size:5..255
@@ -40,6 +39,6 @@ class VacationRequest {
 		approvalRemark nullable:true
 		resendRequest nullable:true
 		dateCreated blank:false
-    lastUpdated nullable:true		
-    }
+		lastUpdated nullable:true
+	}
 }
