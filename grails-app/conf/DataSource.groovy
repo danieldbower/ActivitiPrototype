729793994c5719e20b4e075cd1eb63e3f0ed4c07
Daniel Bower <daniel.bower@infinum.com>
2013-11-20 11:34:36 -0500

    revert previous, use /dbconsole url instead

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -14,8 +14,8 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            url = "jdbc:h2:tcp://localhost/activiti"
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            //url = "jdbc:h2:tcp://localhost/activiti"
         }
     }
     test {
